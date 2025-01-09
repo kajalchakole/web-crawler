01d412ec8ca299039b9c5f27266fec9f74454d93
Kajal Chakole <kajal.chakole@getvymo.com>
2025-01-09 19:47:31 +0530

    Added Caching using Redis

--- src/robot.js
+++ src/robot.js
@@ -1,14 +1,30 @@
 import axios from 'axios';
 import robotsParser from 'robots-parser';
+import Redis from 'ioredis';
+
+const redis = new Redis(); //Connect to Redis
 
 const fetchRobotsTxt = async (domain) => {
+    const url = `${domain}/robots.txt`;
+    const cacheKey = `robots:${domain}`;
+    const cachedRobotsTxt = await redis.get(cacheKey);
+
+    if(cachedRobotsTxt){
+        console.log(`Using cached robots.txt for ${domain}: ${cachedRobotsTxt}`);
+        return robotsParser(url, cachedRobotsTxt);
+    }
+
     try {
-        const url = `${domain}/robots.txt`;
         const response = await axios.get(url, { timeout: 5000});
+        await redis.setex(cacheKey, 86400, response.data);
+        console.log(`Set robots.txt for ${domain}`);
+        
         return robotsParser(url, response.data);
     } catch (error) {
         console.warn(`Error fetching robots.txt for ${domain}: ${error.message}`);
         const defaultTxt = 'User-agent: *\nAllow: /';
+        await redis.setex(cacheKey, 86400, defaultTxt);
+
         return robotsParser(url, defaultTxt);
     }
 };
